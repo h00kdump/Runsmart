bc88e059897158bcef3a182ab0525ac00666dd2e
Igor <h00kdump@yandex.ru>
2022-07-18 18:39:04 +0300

    add js

---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+
+/* let number = 7;
+const pi = 3.14;
+
+console.log(number); */
+
+/* let answer = confirm('Вам есть 18 лет?');
+console.log(answer); */
+
+/* let answer = prompt('Вам есть 18 лет?','');
+console.log(answer); */
+
+// let isChecked = true,
+//     isClosed = false;
+
+/* console.log(isChecked == isClosed);
+ */
+
+// console.log(isChecked || isClosed);
+
+// if (2*4 == 12*1) {
+//     console.log("Верно");
+// } else {
+//     console.log("Ошибка");
+// }
+
+// let answer = confirm('Вам есть 18 лет?');
+
+// if (answer) {
+//     console.log("Проходите");
+// } else {
+//     console.log ("Уходите");
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log("Неправильно");
+// } else if (num > 100) {
+//     console.log("Много");
+// } else {
+//     console.log("Верно");
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+
+function logging(a,b) {
+    console.log( a + b );
+}
+
+logging(3,8);
+
+logging(28,88);
