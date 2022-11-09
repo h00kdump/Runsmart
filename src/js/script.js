cb28589b632643cb140b2342dea63a0e1f5b1f45
Igor <h00kdump@yandex.ru>
2022-11-09 18:13:44 +0300

    thanks

--- src/js/script.js
+++ src/js/script.js
@@ -152,6 +152,8 @@ $(document).ready(function(){
           data: $(this).serialize()
         }).done(function() {
           $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('fast');
 
           $('form').trigger('reset');
 
